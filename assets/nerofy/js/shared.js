9551abbf77189bcbc6ad0e4136bb491fc146c645
nerotvlive <daniel.niesmann@outlook.com>
2025-07-19 18:30:24 +0200

    Add basic bootstrap loading js

---
+++ assets/nerofy/js/shared.js
@@ -0,0 +1,51 @@
+loadStyle("assets/bootstrap/css/bootstrap.min.css");
+loadScript("assets/bootstrap/js/bootstrap.min.js");
+loadStyle("assets/nerofy/css/shared.css");
+
+let theme = "default";
+if(localStorage.getItem("theme") != null) {
+    theme = localStorage.getItem("theme");
+}
+setTheme(theme);
+
+function setTheme(style) {
+    style = style.toLocaleLowerCase();
+    if(style === "dark" || style === "light") {
+        theme = style;
+    } else {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            style = "dark";
+        } else {
+            style = "light";
+        }
+        theme = "default";
+    }
+    document.body.setAttribute('data-bs-theme', style);
+    localStorage.setItem("theme", style);
+}
+
+
+function loadStyle(src) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = src;
+    document.head.appendChild(link);
+}
+
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.defer = true;
+    script.src = src;
+    document.body.appendChild(script);
+}
+
+function loadBootstrap() {
+    loadScript("assets/popper/dist/umd/popper.min.js");
+    loadScript("assets/bootstrap/js/bootstrap.bundle.min.js");
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+}
+
+document.addEventListener('DOMContentLoaded', loadBootstrap);
